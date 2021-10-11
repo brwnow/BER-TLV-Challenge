2b58027f3b8d8e41b959b49c033f3d70b66f02b5
Bruno Garcia <carlos.garcia@stone.com.br>
2021-10-11 03:49:36 -0300

    Updating BTLV lib doc to API version 0.0.3

--- doc/generated/html/search/all_6.js
+++ doc/generated/html/search/all_6.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['value',['value',['../struct___data_object.html#a9097cccbe0c18c0a9dd3326758defe9f',1,'_DataObject']]]
+  ['tag',['tag',['../struct___data_object.html#a080114c2ba42bca16d514df3d1adfaff',1,'_DataObject']]],
+  ['tagsize',['tagSize',['../struct___data_object.html#a6294c9613a17c680d168830d7f0e44e1',1,'_DataObject']]],
+  ['tlvparser_2eh',['tlvparser.h',['../tlvparser_8h.html',1,'']]],
+  ['type',['type',['../struct___data_object.html#a313145dce23884b1f75c0489d894c0cc',1,'_DataObject']]],
+  ['typehandler_2eh',['typehandler.h',['../typehandler_8h.html',1,'']]]
 ];
---
+++ doc/generated/html/search/all_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['value',['value',['../struct___data_object.html#a9097cccbe0c18c0a9dd3326758defe9f',1,'_DataObject']]]
+];
